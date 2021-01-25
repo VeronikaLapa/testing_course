da1a967f9bfc011385c459aba64fc7d45a05585e
VeronikaLapa <Lapshina.nika@yandex.ru>
2021-01-25 22:16:43 +0300

    Test changed. Added configuration

--- server/src/test/java/com/example/server/JwtServiceTest.java
+++ server/src/test/java/com/example/server/JwtServiceTest.java
@@ -3,15 +3,32 @@ package com.example.server;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.example.server.domain.User;
+import com.example.server.repository.UserRepository;
 import com.example.server.service.JwtService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
+@SpringBootTest
 public class JwtServiceTest {
-
+    @Configuration
+    @Import(JwtService.class)
+    static class TestConfig {
+        @Bean
+        UserRepository userRepository() {
+            return mock(UserRepository.class);
+        }
+    }
     //Я понимаю, что приватные константы нельзя вставлять в другие классы.
     //Сделано исключительно с целью создания теста
 
